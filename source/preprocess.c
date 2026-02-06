a9c261bd97eb46fd00e0798c36448fd5504e9918
Mohammad Amin Tatari <amintatari64@gmail.com>
2026-02-06 21:43:09 +0330

    Preventing price normalization

--- source/preprocess.c
+++ source/preprocess.c
@@ -5,46 +5,49 @@
 void dataset_normalize_min_max(
         Dataset* dataset,
         double* normalization_ratios,
-        double* normalization_biases
+        double* normalization_biases,
+        int price_index
 ) {
     if (!dataset || !dataset->data || dataset->max_rows <= 0 || dataset->max_cols <= 1) {
         return;
     }
 
     const int max_rows = dataset->max_rows;
-    const int num_features = dataset->max_cols - 1;
+    const int num_features = dataset->max_cols;
 
     for (int col = 0; col < num_features; col++) {
-        double min_val = dataset->data[0][col];
-        double max_val = dataset->data[0][col];
+        if(col != price_index){
+            double min_val = dataset->data[0][col];
+            double max_val = dataset->data[0][col];
 
-        for (int row = 1; row < max_rows; row++) {
-            const double v = dataset->data[row][col];
-            if (v < min_val) min_val = v;
-            if (v > max_val) max_val = v;
-        }
+            for (int row = 1; row < max_rows; row++) {
+                const double v = dataset->data[row][col];
+                if (v < min_val) min_val = v;
+                if (v > max_val) max_val = v;
+            }
 
-        if (max_val == min_val) {
-            normalization_ratios[col] = 0.0;
-            normalization_biases[col] = NORM_MIN;
+            if (max_val == min_val) {
+                normalization_ratios[col] = 0.0;
+                normalization_biases[col] = NORM_MIN;
+
+                for (int row = 0; row < max_rows; row++) {
+                    dataset->data[row][col] = NORM_MIN;
+                }
+                continue;
+            }
+
+            const double ratio =
+                    (NORM_MAX - NORM_MIN) / (max_val - min_val);
+            const double bias =
+                    NORM_MIN - min_val * ratio;
+
+            normalization_ratios[col] = ratio;
+            normalization_biases[col] = bias;
 
             for (int row = 0; row < max_rows; row++) {
-                dataset->data[row][col] = NORM_MIN;
+                dataset->data[row][col] =
+                        dataset->data[row][col] * ratio + bias;
             }
-            continue;
-        }
-
-        const double ratio =
-                (NORM_MAX - NORM_MIN) / (max_val - min_val);
-        const double bias =
-                NORM_MIN - min_val * ratio;
-
-        normalization_ratios[col] = ratio;
-        normalization_biases[col] = bias;
-
-        for (int row = 0; row < max_rows; row++) {
-            dataset->data[row][col] =
-                    dataset->data[row][col] * ratio + bias;
         }
     }
 }
